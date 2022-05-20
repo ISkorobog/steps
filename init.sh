39499ac3963dd8e3342aa9638536e65b7a329ad4
skorobog <skorobogatkoi2003@gmail.com>
2022-05-21 01:41:30 +0300

    no message

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx_1_9_11.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
